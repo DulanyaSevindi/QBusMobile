433e1b59c57b818a6070437d6c4acccf9711cb3f
IT21277436 <it21277436@my.sliit.lk>
2023-10-16 21:02:38 +0530

    QR scanner added

--- Navigation/BusHomeScreen.js
+++ Navigation/BusHomeScreen.js
@@ -2,20 +2,13 @@ import { View, Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import COLORS from "../constants/colors";
+import QRCodeScanner from "../components/QRCodeScanner";
 
 const BusHomeScreen = () => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
       <View style={{ flex: 1, marginHorizontal: 22 }}>
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: 400,
-            marginVertical: 8,
-          }}
-        >
-          Bus Screen
-        </Text>
+        <QRCodeScanner />
       </View>
     </SafeAreaView>
   );
