d5ea01cd810a9566ec9fbdc20493d2dd6fb41c09
IT21277436 <it21277436@my.sliit.lk>
2023-10-16 22:56:09 +0530

    Scan Bus

--- Navigation/BusHomeScreen.js
+++ Navigation/BusHomeScreen.js
@@ -1,16 +1,78 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import COLORS from "../constants/colors";
-import QRCodeScanner from "../components/QRCodeScanner";
+// import QRCodeScanner from "../components/QRCodeScanner";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 const BusHomeScreen = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [id, setId] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const createTicket = async () => {
+    try {
+      const response = await axios.post("http://192.168.1.5:4000/api/ticket/", {
+        ticketPrice: 120,
+        distance: "4km",
+        routeNumber: "177",
+        pickup: "Malabe",
+        dropOff: "Kaduwela",
+        user: id,
+      });
+      alert("Sucess.");
+    } catch (error) {
+      console.error("API failed:", error);
+      if (error.response && error.response.status === 400) {
+        console.error(error.response.data.error);
+      }
+    }
+  };
+
+  const handleBarCodeScanned = (data) => {
+    setScanned(true);
+    setId(data);
+    createTicket();
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
       <View style={{ flex: 1, marginHorizontal: 22 }}>
-        <QRCodeScanner />
+        <View style={styles.container}>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+          {scanned && (
+            <Button
+              title="Tap to Scan Again"
+              onPress={() => setScanned(false)}
+            />
+          )}
+        </View>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "flex-end",
+  },
+});
 export default BusHomeScreen;
