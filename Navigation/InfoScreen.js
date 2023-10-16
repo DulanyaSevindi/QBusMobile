1c178e33705ec2a5775dea3686cdb7ec89222b12
IT21217586 <it21217586@my.sliit.lk>
2023-10-16 14:52:10 +0530

    image change

--- Navigation/InfoScreen.js
+++ Navigation/InfoScreen.js
@@ -2,17 +2,17 @@ import { View, Text, Image, ScrollView, FlatList } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import COLORS from "../constants/colors";
-import { Chip } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import { Chip } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const InfoScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
       <View
         style={{
-          height: '40%', // Set the height to 40% of the screen
-          backgroundColor: '#3d73d4',
-          position: 'absolute',
+          height: "40%", // Set the height to 40% of the screen
+          backgroundColor: "#3d73d4",
+          position: "absolute",
           top: 0,
           left: 0,
           right: 0,
@@ -20,22 +20,22 @@ const InfoScreen = ({ navigation }) => {
           borderBottomRightRadius: 30, // Adjust this value as needed for the desired oval shape
         }}
       >
-         <Image
+        <Image
           style={{
-            position: 'absolute',
-            top: 0, // Adjust the position of the image as needed
-            left: 0, // Adjust the position of the image as needed
-            width: '110%', // Adjust the width to zoom the image
-            height: '100%', // Adjust the height to zoom the image
+            position: "relative",
+            bottom: 30, // Adjust the position of the image as needed
+            right: 10, // Adjust the position of the image as needed
+            width: "110%", // Adjust the width to zoom the image
+            height: "100%", // Adjust the height to zoom the image
             opacity: 0.5, // Set the opacity to make the image transparent (0.5 is 50% opacity)
           }}
-          source={require("../assets/map-306920_640.webp")}
+          source={require("../assets/world-mapbackground-removebg-preview.png")}
         />
       </View>
 
       <View
         style={{
-          alignItems: 'center', // Center the content vertically
+          alignItems: "center", // Center the content vertically
           marginHorizontal: 22,
           flexDirection: "row",
           justifyContent: "space-between",
@@ -44,15 +44,14 @@ const InfoScreen = ({ navigation }) => {
         <Text
           style={{
             fontSize: 36,
-            fontWeight: "bold",
+            fontWeight: 800,
             marginVertical: 8,
-            color: 'white',
+            color: "white",
           }}
         >
           QBus
-          
         </Text>
-        
+
         <Image
           style={{
             height: 50,
@@ -64,30 +63,43 @@ const InfoScreen = ({ navigation }) => {
           source={require("../assets/profile.png")}
         />
       </View>
-      
+
       <View
         style={{
           margin: 20,
           paddingTop: 10,
           paddingLeft: 5,
           borderRadius: 40,
-          width: '90%', // Adjust the width to zoom the image
-          height: '30%', // Adjust the height to zoom the image
+          width: "90%", // Adjust the width to zoom the image
+          height: "30%", // Adjust the height to zoom the image
           backgroundColor: "#f0f0f0", // Change the background color to gray
-          alignItems: 'center', // Center the content horizontally
-          shadowColor: 'black', // Shadow color
+          alignItems: "center", // Center the content horizontally
+          shadowColor: "black", // Shadow color
           shadowOffset: { width: 0, height: 4 }, // Shadow offset
           shadowOpacity: 0.5, // Shadow opacity
-          elevation: 5, // Android shadow elevation
+          elevation: 20, // Android shadow elevation
         }}
       >
-         <Text style={{ fontSize: 30, marginVertical: 20, fontWeight: 'bold' }}>
-    Balance
-  </Text>
-
-  <Text style={{ fontSize: 30, fontWeight: "bold" }}>
-    Rs.1,250.40
-  </Text>
+        <Text
+          style={{
+            fontSize: 30,
+            marginVertical: 20,
+            fontWeight: "bold",
+            textDecorationLine: "underline",
+          }}
+        >
+          Balance
+        </Text>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            borderWidth: 1,
+            padding: 10,
+          }}
+        >
+          Rs.1,250.40
+        </Text>
       </View>
 
       <View
@@ -96,14 +108,14 @@ const InfoScreen = ({ navigation }) => {
           paddingTop: 10,
           paddingLeft: 5,
           borderRadius: 40,
-          width: '90%', // Adjust the width to zoom the image
-          height: '50%', // Adjust the height to zoom the image
+          width: "90%", // Adjust the width to zoom the image
+          height: "50%", // Adjust the height to zoom the image
           backgroundColor: "#f0f0f0", // Change the background color to gray
-          alignItems: 'center', // Center the content horizontally
-          shadowColor: 'black', // Shadow color
+          alignItems: "center", // Center the content horizontally
+          shadowColor: "black", // Shadow color
           shadowOffset: { width: 0, height: 4 }, // Shadow offset
           // shadowOpacity: 0.9, // Shadow opacity
-          elevation: 5, // Android shadow elevation
+          elevation: 10, // Android shadow elevation
         }}
       >
         <Text
@@ -126,12 +138,10 @@ const InfoScreen = ({ navigation }) => {
 
       <View
         style={{
-          alignItems: 'center',
+          alignItems: "center",
           margin: 20,
         }}
-      >
-        
-      </View>
+      ></View>
     </SafeAreaView>
   );
 };
